5fae489655e17085f8aa8d3c2624f51822a38738
dezhik74 <dezhik@yandex.ru>
2025-10-07 18:42:00 +0300

    0.1.9 yandex webmaster verification

--- kazan/urls.py
+++ kazan/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from blog.sitemaps import StaticViewSitemap, BlogPostSitemap, LocationSitemap, TagSitemap, AboutPageSitemap
 from blog.views import robots_txt
@@ -18,6 +19,11 @@ sitemaps = {
 urlpatterns = [
     path('robots.txt', robots_txt, name='robots_txt'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path(
+        'yandex_c9d84c93bcab45a5.html',
+        TemplateView.as_view(template_name='verification/yandex_c9d84c93bcab45a5.html'),
+        name='yandex-verification'
+    ),
 
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
